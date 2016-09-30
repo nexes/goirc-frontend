3ea5d8b7dd3b34d19ea409962cc97fde921b5eb5
Joe Berria <joeberria@gmail.com>
2016-09-30 15:15:46 -0600

    holding user input in component state, setup callback function

--- app/component/chatinput.jsx
+++ app/component/chatinput.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 
+
 export class ChatInput extends React.Component {
     constructor(props) {
         super(props);
         
-        //dont need this to be state? dont need to cause a render call
-        this.command = '';
-        this.userInput = this.userInput.bind(this);
+        //hold user input
+        this.state = {
+            command: ''
+        };
+        this.updateUserInput = this.updateUserInput.bind(this);
+        this.submitUserInput = this.submitUserInput.bind(this);
     }
 
-    userInput(e) {
-        this.command = e.target.value;
-        console.log(this.command);
+    updateUserInput(e) {
+        this.setState({command: e.target.value});
+    }
+
+    submitUserInput(event) {
+        if (this.state.command !== '') {
+            this.props.sendInput(this.state.command);
+            this.setState({command: ''});
+        }
     }
 
     render() {
         return (
             <div className="chat-input">
                 <span className="input-name well well-sm">Server:</span>
-                <input type="text" className="input-msg" placeholder="message" onChange={this.userInput}/>
-                <button className="input-send btn btn-primary" onClick={this.props.sendInput}>Send</button>
+                <input type="text" className="input-msg" placeholder="message" value={this.state.command} onChange={this.updateUserInput}/>
+                <button className="input-send btn btn-primary" onClick={this.submitUserInput}>Send</button>
             </div>
         );
     }
